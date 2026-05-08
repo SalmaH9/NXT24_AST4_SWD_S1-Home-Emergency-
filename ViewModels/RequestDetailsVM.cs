0b938740d2c37a8e8c2e00c45003a0c54d5692ee
SalmaH9 <s.h01063083970@gmail.com>
2026-05-08 13:06:37 +0300

    models and views

---
+++ ViewModels/RequestDetailsVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeEmergency.ViewModels
+{
+    internal class RequestDetailsVM
+    {
+        public int RequestId { get; set; }
+        public string UserName { get; set; }
+        public string CategoryName { get; set; }
+
+        public string Description { get; set; }
+        public int RequiredProviders { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
